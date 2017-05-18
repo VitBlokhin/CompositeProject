73839f1612855491325df2986302abb0b43a99b9
vit <vit.blokhin@gmail.com>
2017-05-19 01:29:55 +0300

    Initial commit

--- src/org/itstep/pps2701/blokhin/Main.java
+++ src/org/itstep/pps2701/blokhin/Main.java
@@ -2,6 +2,12 @@ package org.itstep.pps2701.blokhin;
 
 import javax.swing.*;
 
+/*
+    Структурные паттерны - паттерн Компоновщик (Composite)
+    Структура данных для хранения информации о государствах и их административных частях и городах
+
+ */
+
 public class Main {
 
     public static void main(String[] args) {
